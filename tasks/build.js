ad88ca4d51700e8fe81d57b53b28e40b2e772c2d
Jakub Szwacz <jakub@szwacz.com>
2015-05-30 09:55:58 +0200

    Be less strict about place for less files

--- tasks/build.js
+++ tasks/build.js
@@ -99,7 +99,7 @@ gulp.task('finalize', ['clean'], function () {
 gulp.task('watch', function () {
     gulp.watch(paths.jsCodeToTranspile, ['transpile-watch']);
     gulp.watch(paths.toCopy, ['copy-watch']);
-    gulp.watch('app/stylesheets/**/*.less', ['less-watch']);
+    gulp.watch('app/**/*.less', ['less-watch']);
 });
 
 
