06c6a16f98d7d331e360a40a11b3d30a9452a58f
Gabriel Engel <gabriel.engel@fgsys.com>
2015-07-08 18:40:47 -0300

    adding IPC

--- app/main.js
+++ app/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var app = require('app');
+var ipc = require('ipc');
+var path = require('path');
 var BrowserWindow = require('browser-window');
 var env = require('./vendor/electron_boilerplate/env_config');
 var devHelper = require('./vendor/electron_boilerplate/dev_helper');
 var windowStateKeeper = require('./vendor/electron_boilerplate/window_state');
 
+// global variable
+var APP_NAME = 'Rocket.Chat';
+var INDEX = 'https://rocket.chat/home';
+// var INDEX = 'file://' + path.join( __dirname, 'app.html' );
+
+// Create main window
 var mainWindow;
 
 // Preserver of the window size and position between app launches.
@@ -14,12 +22,21 @@ var mainWindowState = windowStateKeeper('main', {
     height: 600
 });
 
-app.on('ready', function() {
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', appReady);
+
+function appReady() {
 
     mainWindow = new BrowserWindow({
+        title: APP_NAME,
         "node-integration": false,
         'accept-first-mouse': true,
-        title: 'Rocket.Chat',
         show: false,
         x: mainWindowState.x,
         y: mainWindowState.y,
@@ -31,13 +48,15 @@ app.on('ready', function() {
         mainWindow.maximize();
     }
 
-    // mainWindow.loadUrl('file://' + __dirname + '/app.html');
-    mainWindow.loadUrl('https://rocket.chat/home');
+    mainWindow.loadUrl(INDEX);
 
     mainWindow.webContents.on('did-finish-load', function() {
         mainWindow.show();
     });
 
+    mainWindow.webContents.gabriel = 'TEST';
+    console.log(mainWindow.webContents);
+    console.log(ipc);
 
     if (env.name === 'development') {
         devHelper.setDevMenu();
@@ -47,8 +66,15 @@ app.on('ready', function() {
     mainWindow.on('close', function() {
         mainWindowState.saveState(mainWindow);
     });
+
+};
+
+// Custom function
+ipc.on('message', function(event, arg) {
+    console.log(arg); // prints "ping"
+    event.returnValue = 'pong';
 });
 
-app.on('window-all-closed', function() {
-    app.quit();
+ipc.on('open-dev', function(event, arg) {
+    mainWindow.openDevTools();
 });
