8d01ee6a8895fe6ac30261870de9b4fed2993453
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2015-07-10 09:01:46 +0200

    #1 add network check + rocket.chat header check

--- app/scripts/start.js
+++ app/scripts/start.js
@@ -1,5 +1,6 @@
 (function() {
-    var key = 'rocket.chat.host';
+    var key = 'rocket.chat.host',
+        header = 'X-Rocket-Chat-Version'.toLowerCase();
 
     //init loader
     var loader = document.querySelector('.loader');
@@ -10,7 +11,7 @@
         http.onreadystatechange = function() {
             if (this.readyState == this.DONE) {
                 if (this.response) {
-                    loader.innerHTML = this.response;
+                    loader.innerHTML = this.response + loader.innerHTML;
                 }
             }
         };
@@ -26,6 +27,20 @@
         return;
     }
 
+    // connection check
+    if (!navigator.onLine) offline();
+    window.addEventListener('online', online);
+    window.addEventListener('offline', offline);
+
+    function online() {
+        document.body.classList.remove('offline');
+    }
+
+    function offline() {
+        document.body.classList.add('offline');
+    }
+    // end connection check
+
     var form = document.querySelector('form');
     form.addEventListener('submit', function(ev) {
         ev.preventDefault();
@@ -37,31 +52,48 @@
         var url = input.value;
         console.debug('checking', url);
         input.classList.remove('wrong');
-        urlExists(url, function(check) {
-            if (check) {
-                console.debug('url found!');
-                localStorage.setItem(key, url);
-                document.body.classList.add('connecting');
-                // redirect to host
-                redirect(url);
-            } else {
-                button.value = val;
-                console.debug('url wrong');
-                input.classList.add('wrong');
-            }
+        urlExists(url, 5000).then(function() {
+            console.debug('url found!');
+            localStorage.setItem(key, url);
+            document.body.classList.add('connecting');
+            // redirect to host
+            redirect(url);
+        }, function(status) {
+            button.value = val;
+            console.debug('url wrong');
+            input.classList.add('wrong');
         });
         return false;
     });
 
-    function urlExists(url, callback) {
-        var http = new XMLHttpRequest();
-        http.open('HEAD', url);
-        http.onreadystatechange = function() {
-            if (this.readyState == this.DONE) {
-                callback(this.status != 404 && this.status != 0);
+    function urlExists(url, timeout) {
+        return new Promise(function(resolve, reject) {
+            var http = new XMLHttpRequest();
+            var resolved = false;
+            http.open('HEAD', url);
+            http.onreadystatechange = function() {
+                if (this.readyState == this.DONE) {
+                    if (!resolved) {
+                        resolved = true;
+                        var headers = this.getAllResponseHeaders().toLowerCase();
+                        if (headers.indexOf(header) !== -1) {
+                            resolve();
+                        } else {
+                            reject(this.status);
+                        }
+                    }
+                }
+            };
+            if (timeout) {
+                setTimeout(function() {
+                    if (!resolved) {
+                        resolved = true;
+                        reject();
+                    }
+                }, timeout);
             }
-        };
-        http.send();
+            http.send();
+        });
     }
 
     function redirect(url) {
