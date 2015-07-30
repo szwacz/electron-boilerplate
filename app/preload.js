4d863297eb5042aa001029d3149e292f33fe9410
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-07-30 16:12:07 -0300

    Open http links as external

--- app/preload.js
+++ app/preload.js
@@ -9,3 +9,31 @@ events.forEach(function(e) {
 });
 
 require('./menus');
+
+var shell = require('shell');
+
+var supportExternalLinks = function (e) {
+	var href;
+	var isExternal = false;
+
+	var checkDomElement = function (element) {
+		if (element.nodeName === 'A') {
+			href = element.getAttribute('href') || '';
+		}
+
+		if (/^https?:\/\/.+/.test(href) === true /*&& RegExp('^https?:\/\/'+location.host).test(href) === false*/) {
+			isExternal = true;
+		}
+
+		if (href && isExternal) {
+			shell.openExternal(href);
+			e.preventDefault();
+		} else if (element.parentElement) {
+			checkDomElement(element.parentElement);
+		}
+	}
+
+	checkDomElement(e.target);
+}
+
+document.addEventListener('click', supportExternalLinks, false);
