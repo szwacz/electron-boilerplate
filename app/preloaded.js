39f2f5eb73070e0d11b5f94dd92b18958d0e5f13
Antoine Pairet <antoine.pairet@gmail.com>
2015-09-12 21:24:31 +0200

    Fixes #37: Preload script must not be transpiled
    See #37

---
+++ app/preloaded.js
@@ -0,0 +1,4 @@
+// This script will be loaded before any other script in the renderer
+// It is particularly useful when wrapping an existing website inside an electron application
+// This file must not be transpiled.
+window.preloaded = true;
