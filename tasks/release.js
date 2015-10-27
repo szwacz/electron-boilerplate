67b5e2c472444c85eed7db94fac72e4f286a79d6
mc007 <asd28071977>
2015-10-27 15:33:06 +0100

    =auto-commit

--- tasks/release.js
+++ tasks/release.js
@@ -6,7 +6,7 @@ var utils = require('./utils');
 var releaseForOs = {
     osx: require('./release_osx'),
     linux: require('./release_linux'),
-    windows: require('./release_windows'),
+    windows: require('./release_windows')
 };
 
 gulp.task('release', ['build'], function () {
