cb6d6116d78a6f79a586f7a102d3d181cf275e83
mc007 <asd28071977>
2015-10-27 17:20:02 +0100

    =auto-commit

--- app/vendor/electron_boilerplate/dev_helper.js
+++ app/vendor/electron_boilerplate/dev_helper.js
@@ -4,29 +4,56 @@ var app = require('app');
 var Menu = require('menu');
 var BrowserWindow = require('browser-window');
 
+
 module.exports.setDevMenu = function () {
-    var devMenu = Menu.buildFromTemplate([{
-        label: 'Development',
-        submenu: [{
-            label: 'Reload',
-            accelerator: 'F5',
-            click: function () {
-                BrowserWindow.getFocusedWindow().reloadIgnoringCache();
-            }
+
+    var _lastWindow = null;
+
+    var devMenu = Menu.buildFromTemplate([
+        {
+            label: 'Development',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'F5',
+                    click: function () {
+                        BrowserWindow.getFocusedWindow().reloadIgnoringCache();
+                    }
+                },
+                {
+                    label: 'Toggle DevTools',
+                    accelerator: 'F12',
+                    click: function () {
+                        if(BrowserWindow.getFocusedWindow() || _lastWindow) {
+
+                            _lastWindow = BrowserWindow.getFocusedWindow();
+
+                            _lastWindow.toggleDevTools();
+                        }
+                    }
+                },
+                {
+                    label: 'Quit',
+                    accelerator: 'CmdOrCtrl+Q',
+                    click: function () {
+                        app.quit();
+                    }
+                }
+            ]
         },
-            {
-            label: 'Toggle DevTools',
-            accelerator: 'F12',
-            click: function () {
-                BrowserWindow.getFocusedWindow().toggleDevTools();
-            }
-        },{
-            label: 'Quit',
-            accelerator: 'CmdOrCtrl+Q',
-            click: function () {
-                app.quit();
-            }
-        }]
-    }]);
+        {
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Fullscreen',
+                    accelerator: 'F11',
+                    click: function () {
+                        var win = BrowserWindow.getFocusedWindow();
+                        win.setFullScreen(!win.isFullScreen());
+                    }
+                }
+            ]
+        }
+    ]);
     Menu.setApplicationMenu(devMenu);
 };
