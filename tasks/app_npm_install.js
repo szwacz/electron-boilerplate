7370b7b038855bdf3a83410913bbbbcdd17a7e12
Jakub Szwacz <jakub@szwacz.com>
2015-11-11 10:51:32 +0100

    Documentation tweaks

--- tasks/app_npm_install.js
+++ tasks/app_npm_install.js
@@ -1,8 +1,9 @@
 // This script allows you to install native modules (those which have
 // to be compiled) for your Electron app.
 // The problem is that 'npm install' compiles them against node.js you have
-// installed on your computer, NOT against node.js used in Electron
-// runtime we've downloaded to this project.
+// installed on your computer, NOT against node.js used inside Electron.
+// To compile them agains Electron we need to tell npm it explicitly (and
+// that's what this script does).
 
 'use strict';
 
@@ -17,30 +18,30 @@ var electronVersion = utils.getElectronVersion();
 var nodeModulesDir = jetpack.cwd(__dirname + '/../app/node_modules')
 var dependenciesCompiledAgainst = nodeModulesDir.read('electron_version');
 
-// When you raised version of Electron used in your project, the safest
+// When you raise version of Electron used in your project, the safest
 // thing to do is remove all installed dependencies and install them
 // once again (so they compile against new version if you use any
 // native package).
 if (electronVersion !== dependenciesCompiledAgainst) {
     nodeModulesDir.dir('.', { empty: true });
+    // Save the version string in file next to all installed modules so we know
+    // in the future what version of Electron has been used to compile them.
     nodeModulesDir.write('electron_version', electronVersion);
 }
 
-// Tell the 'npm install' which is about to start that we want for it
-// to compile for Electron.
+// Tell the 'npm install' that we want to compile for Electron.
 process.env.npm_config_disturl = "https://atom.io/download/atom-shell";
 process.env.npm_config_target = electronVersion;
 
 var params = ['install'];
-// Maybe there was name of package user wants to install passed as a parameter.
+// Maybe there was name of package user wants to install passed
+// as a parameter to this script.
 if (argv._.length > 0) {
     params.push(argv._[0]);
     params.push('--save');
 }
 
-
-var installCommand = null;
-
+var installCommand;
 if (process.platform === 'win32') {
   installCommand = 'npm.cmd'
 } else {
