e1dec7371041ac0cd7694625d30296a8c8f008a5
Jakub Szwacz <jakub@szwacz.com>
2015-11-15 08:10:28 +0100

    App switched to es6 modules imports

--- app/app.js
+++ app/app.js
@@ -1,19 +1,18 @@
 // Here is the starting point for code of your own application.
 // All stuff below is just to show you how it works. You can delete all of it.
 
-// Modules which you authored in this project are intended to be
-// imported through new ES6 syntax.
-import { greet } from './hello_world/hello_world';
+// Use new ES6 modules syntax for everything.
+import os from 'os'; // native node.js module
+import remote from 'remote'; // native electron module
+import jetpack from 'fs-jetpack'; // module loaded from npm
+import { greet } from './hello_world/hello_world'; // code in this project
 
-// Node.js modules and those from npm
-// are required the same way as always.
-var os = require('os');
-var app = require('remote').require('app');
-var jetpack = require('fs-jetpack').cwd(app.getAppPath());
+var app = remote.require('app');
+var appDir = jetpack.cwd(app.getAppPath());
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
-console.log(jetpack.read('package.json', 'json'));
+console.log(appDir.read('package.json', 'json'));
 
 // window.env contains data from config/env_XXX.json file.
 var envName = window.env.name;
