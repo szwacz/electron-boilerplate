32f9c3686878f1d70dfac2dd10e16a21bdf5fecc
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-03-01 16:19:00 -0300

    Convert identation to tabs

--- app/scripts/preload.js
+++ app/scripts/preload.js
@@ -4,17 +4,17 @@
 var IPC = require('electron').ipcRenderer;
 
 class Notification extends window.Notification {
-    get onclick() {
-        return super.onclick;
-    }
+	get onclick() {
+		return super.onclick;
+	}
 
-    set onclick(fn) {
-        var result = super.onclick = () => {
-            IPC.sendToHost('focus');
-            fn.apply(this, arguments);
-        };
-        return result;
-    }
+	set onclick(fn) {
+		var result = super.onclick = () => {
+			IPC.sendToHost('focus');
+			fn.apply(this, arguments);
+		};
+		return result;
+	}
 }
 
 window.Notification = Notification;
@@ -22,48 +22,48 @@ window.Notification = Notification;
 var events = ['unread-changed'];
 
 events.forEach(function(e) {
-    window.addEventListener(e, function(event) {
-        IPC.sendToHost(e, event.detail);
-    });
+	window.addEventListener(e, function(event) {
+		IPC.sendToHost(e, event.detail);
+	});
 });
 
 window.addEventListener('load', function() {
-    Meteor.startup(function() {
-        Tracker.autorun(function() {
-            var siteName = RocketChat.settings.get('Site_Name');
-            if (siteName) {
-                IPC.sendToHost('title-changed', siteName);
-            }
-        });
-    });
+	Meteor.startup(function() {
+		Tracker.autorun(function() {
+			var siteName = RocketChat.settings.get('Site_Name');
+			if (siteName) {
+				IPC.sendToHost('title-changed', siteName);
+			}
+		});
+	});
 });
 
 var shell = require('shell');
 
 var supportExternalLinks = function(e) {
-    var href;
-    var isExternal = false;
+	var href;
+	var isExternal = false;
 
-    var checkDomElement = function(element) {
-        if (element.nodeName === 'A') {
-            if (element.classList.contains('swipebox') === false) {
-                href = element.getAttribute('href') || '';
-            }
-        }
+	var checkDomElement = function(element) {
+		if (element.nodeName === 'A') {
+			if (element.classList.contains('swipebox') === false) {
+				href = element.getAttribute('href') || '';
+			}
+		}
 
-        if (/^https?:\/\/.+/.test(href) === true /*&& RegExp('^https?:\/\/'+location.host).test(href) === false*/ ) {
-            isExternal = true;
-        }
+		if (/^https?:\/\/.+/.test(href) === true /*&& RegExp('^https?:\/\/'+location.host).test(href) === false*/ ) {
+			isExternal = true;
+		}
 
-        if (href && isExternal) {
-            shell.openExternal(href);
-            e.preventDefault();
-        } else if (element.parentElement) {
-            checkDomElement(element.parentElement);
-        }
-    };
+		if (href && isExternal) {
+			shell.openExternal(href);
+			e.preventDefault();
+		} else if (element.parentElement) {
+			checkDomElement(element.parentElement);
+		}
+	};
 
-    checkDomElement(e.target);
+	checkDomElement(e.target);
 };
 
 document.addEventListener('click', supportExternalLinks, false);
@@ -76,78 +76,78 @@ var menu = new Menu();
 
 // set the initial context menu so that a context menu exists even before spellcheck is called
 var getTemplate = function() {
-    return [
-        {
-            label: 'Undo',
-            role: 'undo'
-        },
-        {
-            label: 'Redo',
-            role: 'redo'
-        },
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Cut',
-            role: 'cut'
-        },
-        {
-            label: 'Copy',
-            role: 'copy'
-        },
-        {
-            label: 'Paste',
-            role: 'paste'
-        },
-        {
-            label: 'Select All',
-            role: 'selectall'
-        }
-    ];
+	return [
+		{
+			label: 'Undo',
+			role: 'undo'
+		},
+		{
+			label: 'Redo',
+			role: 'redo'
+		},
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Cut',
+			role: 'cut'
+		},
+		{
+			label: 'Copy',
+			role: 'copy'
+		},
+		{
+			label: 'Paste',
+			role: 'paste'
+		},
+		{
+			label: 'Select All',
+			role: 'selectall'
+		}
+	];
 };
 
 var template = getTemplate();
 menu = Menu.buildFromTemplate(getTemplate());
 
 window.addEventListener('contextmenu', function(){
-    menu.popup(remote.getCurrentWindow());
-    menu = Menu.buildFromTemplate(template);
+	menu.popup(remote.getCurrentWindow());
+	menu = Menu.buildFromTemplate(template);
 }, false);
 
 try {
-    var checker = require('spellchecker');
+	var checker = require('spellchecker');
 
-    webFrame.setSpellCheckProvider((localStorage.getItem('userLanguage') || 'en'), false, {
-        spellCheck: function(text) {
-            if (localStorage.getItem('userLanguage') && checker.getAvailableDictionaries().length > 0) {
-                checker.setDictionary(localStorage.getItem('userLanguage'));
-            }
+	webFrame.setSpellCheckProvider((localStorage.getItem('userLanguage') || 'en'), false, {
+		spellCheck: function(text) {
+			if (localStorage.getItem('userLanguage') && checker.getAvailableDictionaries().length > 0) {
+				checker.setDictionary(localStorage.getItem('userLanguage'));
+			}
 
-            var isMisspelled = checker.isMisspelled(text);
+			var isMisspelled = checker.isMisspelled(text);
 
-            if (isMisspelled) {
-                var options = checker.getCorrectionsForMisspelling(text);
-                var maxItems = Math.min(options.length, 5);
-                template.unshift({type: 'separator'});
-                if (maxItems > 0) {
-                    var onClick = function(menuItem) {
-                        webContents.replaceMisspelling(menuItem.label);
-                    };
-                    for (var i = maxItems-1; i >= 0; i--) {
-                        var item = options[i];
-                        template.unshift({ label: item, click: onClick});
-                    }
-                } else {
-                    template.unshift({ label: 'no suggestions', click: function() { } });
-                }
-            }
-            menu = Menu.buildFromTemplate(template);
-            template = getTemplate();
+			if (isMisspelled) {
+				var options = checker.getCorrectionsForMisspelling(text);
+				var maxItems = Math.min(options.length, 5);
+				template.unshift({type: 'separator'});
+				if (maxItems > 0) {
+					var onClick = function(menuItem) {
+						webContents.replaceMisspelling(menuItem.label);
+					};
+					for (var i = maxItems-1; i >= 0; i--) {
+						var item = options[i];
+						template.unshift({ label: item, click: onClick});
+					}
+				} else {
+					template.unshift({ label: 'no suggestions', click: function() { } });
+				}
+			}
+			menu = Menu.buildFromTemplate(template);
+			template = getTemplate();
 
-            return !isMisspelled;
-        }
-    });
+			return !isMisspelled;
+		}
+	});
 } catch(e) {
-    console.log('Spellchecker unavailble');
+	console.log('Spellchecker unavailble');
 }
