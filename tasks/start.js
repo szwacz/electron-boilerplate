2120cdb500e953c1895560e453ca907977da254c
Jakub Szwacz <jakub@szwacz.com>
2016-03-14 22:49:13 +0100

    Shorter start task code

--- tasks/start.js
+++ tasks/start.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var electron = require('electron-prebuilt');
 var childProcess = require('child_process');
-var utils = require('./utils');
+var electron = require('electron-prebuilt');
 var gulp = require('gulp');
 
 gulp.task('start', ['build', 'watch'], function () {
-    var app = childProcess.spawn(electron, ['./build'], {
+    childProcess.spawn(electron, ['./build'], {
         stdio: 'inherit'
-    });
-
-    app.on('close', function (code) {
+    })
+    .on('close', function () {
         // User closed the app. Kill the host process.
         process.exit();
     });
