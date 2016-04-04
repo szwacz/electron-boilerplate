e6ca5ad86612bf1c5c1f301f2a8512581a277328
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-04-04 10:10:26 -0300

    Allow to define custom name format

--- tasks/build/build.js
+++ tasks/build/build.js
@@ -21,13 +21,13 @@ var paths = {
         './**/*.html',
         './**/*.+(jpg|png|svg)'
     ],
-}
+};
 
 // -------------------------------------
 // Tasks
 // -------------------------------------
 
-gulp.task('clean', function (callback) {
+gulp.task('clean', function () {
     return destDir.dirAsync('.', { empty: true });
 });
 
--- tasks/release/osx.js
+++ tasks/release/osx.js
@@ -54,7 +54,6 @@ var finalize = function () {
         copyright: manifest.copyright,
         LSApplicationCategoryType: manifest.LSApplicationCategoryType
     });
-
     finalAppDir.write('Contents/Info.plist', info);
 
     // Prepare Info.plist of Helper apps
@@ -143,7 +142,7 @@ var packToDmgFile = function () {
     var deferred = Q.defer();
 
     var appdmg = require('appdmg');
-    var dmgName = manifest.name + '_' + manifest.version + '.dmg';
+    var dmgName = utils.getReleasePackageName(manifest) + '.dmg';
 
     // Prepare appdmg config
     var dmgManifest = projectDir.read('resources/osx/appdmg.json');
@@ -158,7 +157,7 @@ var packToDmgFile = function () {
     // Delete DMG file with this name if already exists
     releasesDir.remove(dmgName);
 
-    gulpUtil.log('Packaging to DMG file...');
+    gulpUtil.log('Packaging to DMG file... (' + dmgName + ')');
 
     var readyDmgPath = releasesDir.path(dmgName);
     appdmg({
