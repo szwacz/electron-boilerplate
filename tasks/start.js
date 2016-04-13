331cc5055f2221879cd97de15baff4275ea200a6
David Knape <github@bumpslide.com>
2016-04-13 14:56:29 -0700

    Simplified BrowserSync integration. browser-sync is no longer a dependency but will be used if found.

--- tasks/start.js
+++ tasks/start.js
@@ -4,39 +4,89 @@ var childProcess = require('child_process');
 var electron = require('electron-prebuilt');
 var gulp = require('gulp');
 var extend = require('util')._extend;
-var utils = require('./utils');
+var util = require("gulp-util");
 
-var buildPath = './build';
+// app environment variables
+var app_env = extend({}, process.env);
 
+// app location
+var build_path = './build';
+
+// browser sync configuration
+var bs_options = {
+    port: 3002,
+    ghostMode: false,
+    logSnippet: false,
+    open: false,
+    notify: false,
+    ui: false,
+    socket: {domain: getBrowserSyncRootUrl}
+};
+
+// Build and run the Electron App and rebuild when files change
 gulp.task('start', ['build', 'watch'], function () {
 
-    utils.initBrowserSync({port: 3002}, function(bs) {
+    // BrowserSync
+    var bs;
 
-        var env = extend({}, process.env);
+    // Using BrowserSync (optional)
+    if (bs = initBrowserSync()) {
+        // Add BrowserSync Client URL to app environment
+        app_env.BROWSER_SYNC_CLIENT_URL = getBrowserSyncClientUrl(bs.instance.options);
 
-        if(bs && bs.active) {
+        // reload on HTML and JS changes
+        bs.watch([
+            build_path + '/app.js',
+            build_path + '/*.html'
+        ]).on('change', bs.reload);
 
-            // Add BrowserSync Client URL to app environment
-            env.BROWSER_SYNC_CLIENT_URL = utils.getBrowserSyncClientUrl(bs.instance.options);
+        // Stream the CSS updates
+        var css = build_path + '/stylesheets/*.css';
+        bs.watch(css).on('change', function () {
+            gulp.src(css).pipe(bs.stream());
+        });
+    }
 
-            // watch for HTML and JS changes
-            bs.watch([buildPath+'/app.js', buildPath+'/*.html']).on('change', bs.reload);
-
-            // Stream the CSS updates
-            var css = buildPath+'/stylesheets/*.css';
-            bs.watch(css).on('change', function () {
-                gulp.src(css).pipe(bs.stream());
-            });
-        }
-
-        // start the electron app
-        childProcess.spawn(electron, [buildPath], {
-                stdio: 'inherit',
-                env: env
-            })
-            .on('close', function () {
-                // User closed the app. Kill the host process.
-                process.exit();
-            });
+    // start the electron app
+    childProcess.spawn(electron, [build_path], {
+        stdio: 'inherit',
+        env: app_env
+    }).on('close', function () {
+        // User closed the app. Kill the host process.
+        process.exit();
     });
 });
+
+
+// Setup BrowserSync and catch errors gracefully
+function initBrowserSync() {
+    try {
+        try {
+            // Load on demand
+            var BrowserSync = require("browser-sync");
+        } catch (e) {
+            // Fail politely
+            util.log('Note: BrowserSync is available. "npm install browser-sync" to enable it.');
+            return false;
+        }
+        // Create, initialize, and return a BrowserSync instance
+        var bs = BrowserSync.create();
+        bs.init(bs_options);
+        return bs;
+    } catch (e) {
+        // Bad options, or something...
+        console.error('Error starting BrowserSync', e);
+        return false;
+    }
+}
+
+// BrowserSync URL root
+function getBrowserSyncRootUrl() {
+    return 'http://localhost:' + bs_options.port;
+}
+
+// BrowserSync JS client URL
+function getBrowserSyncClientUrl(options) {
+    var connectUtils = require('browser-sync/lib/connect-utils');
+    return getBrowserSyncRootUrl() + connectUtils.clientScript(options);
+}
--- tasks/utils.js
+++ tasks/utils.js
@@ -2,7 +2,6 @@
 
 var argv = require('yargs').argv;
 var os = require('os');
-var extend = require('util')._extend;
 
 module.exports.os = function () {
     switch (os.platform()) {
@@ -51,39 +50,3 @@ module.exports.spawnablePath = function (path) {
     }
     return path;
 };
-
-
-// return BrowserSync URL root
-var getBrowserSyncRootUrl = module.exports.getBrowserSyncRootUrl = function (options) {
-    return 'http://localhost:' + options.get('port');
-};
-
-// return BrowserSync JS client URL
-module.exports.getBrowserSyncClientUrl = function (options) {
-    var connectUtils = require('browser-sync/lib/connect-utils');
-    return getBrowserSyncRootUrl(options) + connectUtils.clientScript(options);
-};
-
-// Setup BrowserSync and catch errors gracefully
-module.exports.initBrowserSync = function (bs_opts, ready) {
-    var bs = require("browser-sync").create();
-    var opts = extend({
-        ui: false,
-        port: 35829,
-        ghostMode: false,
-        open: false,
-        notify: false,
-        logSnippet: false,
-        socket: {domain: getBrowserSyncRootUrl}
-    }, bs_opts);
-    var err;
-    try {
-        bs.init(opts, function (){
-            ready( err ? null : bs);
-        });
-    } catch (e) {
-        err = e;
-        console.error('Error starting BrowserSync', e);
-    }
-};
-
