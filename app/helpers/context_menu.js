10a9dd868ce0c9728278f22f83a45f0c95be8e03
Jakub Szwacz <jakub@szwacz.com>
2016-04-16 15:17:25 +0200

    Show 'copy' in context menu when any text selected

--- app/helpers/context_menu.js
+++ app/helpers/context_menu.js
@@ -8,6 +8,10 @@
     var Menu = remote.Menu;
     var MenuItem = remote.MenuItem;
 
+    var isAnyTextSelected = function () {
+        return window.getSelection().toString() !== '';
+    };
+
     var cut = new MenuItem({
         label: "Cut",
         click: function () {
@@ -29,21 +33,27 @@
         }
     });
 
-    var textMenu = new Menu();
-    textMenu.append(cut);
-    textMenu.append(copy);
-    textMenu.append(paste);
+    var normalMenu = new Menu();
+    normalMenu.append(copy);
+
+    var textEditingMenu = new Menu();
+    textEditingMenu.append(cut);
+    textEditingMenu.append(copy);
+    textEditingMenu.append(paste);
 
     document.addEventListener('contextmenu', function (e) {
-
         switch (e.target.nodeName) {
             case 'TEXTAREA':
             case 'INPUT':
                 e.preventDefault();
-                textMenu.popup(remote.getCurrentWindow());
+                textEditingMenu.popup(remote.getCurrentWindow());
                 break;
+            default:
+                if (isAnyTextSelected()) {
+                    e.preventDefault();
+                    normalMenu.popup(remote.getCurrentWindow());
+                }
         }
-
     }, false);
 
 }());
