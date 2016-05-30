fcc2327c5580ee406c50e5cb2888a049666cdf27
Jakub Szwacz <jakub@szwacz.com>
2016-05-30 22:49:12 +0200

    Correct string replace pattern

--- tasks/build/generate_spec_imports.js
+++ tasks/build/generate_spec_imports.js
@@ -18,7 +18,7 @@ module.exports = function () {
     return srcDir.findAsync('.', { matching: whatToInclude })
         .then(function (specPaths) {
             var fileContent = specPaths.map(function (path) {
-                return 'import "./' + path.replace('\\', '/') + '";';
+                return 'import "./' + path.replace(/\\/g, '/') + '";';
             }).join('\n');
             return srcDir.writeAsync(fileName, fileBanner + fileContent);
         })
