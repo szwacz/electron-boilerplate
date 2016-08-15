be5d1de25cf4fd46fa9ac05e96721642a6ad5d3f
Jakub Szwacz <jakub@szwacz.com>
2016-08-15 07:52:51 +0200

    Be more tolerant to e2e timeouts

--- e2e/utils.js
+++ e2e/utils.js
@@ -2,15 +2,18 @@ import electron from 'electron';
 import { Application } from 'spectron';
 
 var beforeEach = function () {
+    this.timeout(10000);
     this.app = new Application({
         path: electron,
         args: ['app'],
+        startTimeout: 10000,
         waitTimeout: 10000,
     });
     return this.app.start();
 };
 
 var afterEach = function () {
+    this.timeout(10000);
     if (this.app && this.app.isRunning()) {
         return this.app.stop();
     }
