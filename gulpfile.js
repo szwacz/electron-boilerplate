4def69820b3b16af010826999591423d4feb8c7f
zhangaz1 <zhangaz1@hotmail.com>
2017-10-31 19:54:26 +0800

    add test task in gulp and watches for test

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 require('./tasks/build_app');
 require('./tasks/build_tests');
 require('./tasks/start');
+require('./tasks/test');
--- tasks/build_app.js
+++ tasks/build_app.js
@@ -12,40 +12,33 @@ const srcDir = jetpack.cwd('./src');
 const destDir = jetpack.cwd('./app');
 
 gulp.task('bundle', () => {
-  return Promise.all([
-    bundle(srcDir.path('background.js'), destDir.path('background.js')),
-    bundle(srcDir.path('app.js'), destDir.path('app.js')),
-  ]);
+	return Promise.all([
+		bundle(srcDir.path('background.js'), destDir.path('background.js')),
+		bundle(srcDir.path('app.js'), destDir.path('app.js')),
+	]);
 });
 
 gulp.task('less', () => {
-  return gulp.src(srcDir.path('stylesheets/main.less'))
-  .pipe(plumber())
-  .pipe(less())
-  .pipe(gulp.dest(destDir.path('stylesheets')));
+	return gulp.src(srcDir.path('stylesheets/main.less'))
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(gulp.dest(destDir.path('stylesheets')));
 });
 
 gulp.task('environment', () => {
-  const configFile = `config/env_${utils.getEnvName()}.json`;
-  projectDir.copy(configFile, destDir.path('env.json'), { overwrite: true });
+	const configFile = `config/env_${utils.getEnvName()}.json`;
+	projectDir.copy(configFile, destDir.path('env.json'), {
+		overwrite: true
+	});
 });
 
 gulp.task('watch', () => {
-  const beepOnError = (done) => {
-    return (err) => {
-      if (err) {
-        utils.beepSound();
-      }
-      done(err);
-    };
-  };
-
-  watch('src/**/*.js', batch((events, done) => {
-    gulp.start('bundle', beepOnError(done));
-  }));
-  watch('src/**/*.less', batch((events, done) => {
-    gulp.start('less', beepOnError(done));
-  }));
+	watch('src/**/*.js', batch((events, done) => {
+		gulp.start('bundle', utils.beepOnError(done));
+	}));
+	watch('src/**/*.less', batch((events, done) => {
+		gulp.start('less', utils.beepOnError(done));
+	}));
 });
 
 gulp.task('build', ['bundle', 'less', 'environment']);
