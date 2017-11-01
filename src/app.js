826342ca4e78ba59934b5a720ae753342f9ae46c
Jakub Szwacz <jakub@szwacz.com>
2017-11-01 22:00:23 +0100

    Comments amendments

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import "./stylesheets/main.css";
 import "./helpers/context_menu.js";
 import "./helpers/external_links.js";
 
+// ----------------------------------------------------------------------------
 // Everything below is just to show you how it works. You can delete all of it.
+// ----------------------------------------------------------------------------
+
 import { remote } from "electron";
 import jetpack from "fs-jetpack";
 import { greet } from "./hello_world/hello_world";
@@ -14,7 +17,7 @@ const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
-// here files from disk like it's node.js! Welcome to Electron world :)
+// files from disk like it's node.js! Welcome to Electron world :)
 const manifest = appDir.read("package.json", "json");
 
 const osMap = {
--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 // This is main process of Electron, started as first thing when your
-// app starts. This script is running through entire life of your application.
+// app starts. It runs through entire life of your application.
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
 
