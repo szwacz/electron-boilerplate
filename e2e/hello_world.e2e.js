354c2fa904a8cbc00accb748c73ecbe57ed71215
Jakub Szwacz <jakub@szwacz.com>
2019-07-30 21:26:07 +0200

    uncomment test

--- e2e/hello_world.e2e.js
+++ e2e/hello_world.e2e.js
@@ -2,12 +2,12 @@ import { expect } from "chai";
 import testUtils from "./utils";
 
 describe("application launch", () => {
-  // beforeEach(testUtils.beforeEach);
-  // afterEach(testUtils.afterEach);
-  //
-  // it("shows hello world text on screen after launch", function() {
-  //   return this.app.client.getText("#greet").then(text => {
-  //     expect(text).to.equal("Hello World!");
-  //   });
-  // });
+  beforeEach(testUtils.beforeEach);
+  afterEach(testUtils.afterEach);
+
+  it("shows hello world text on screen after launch", function() {
+    return this.app.client.getText("#greet").then(text => {
+      expect(text).to.equal("Hello World!");
+    });
+  });
 });
