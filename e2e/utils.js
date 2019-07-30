9b4bfbd13eee39a28298469d51cfbe763301159c
Jakub Szwacz <jakub@szwacz.com>
2019-07-30 21:32:07 +0200

    Update dependencies to electron v4 (#307)

--- e2e/utils.js
+++ e2e/utils.js
@@ -2,18 +2,14 @@ import electron from "electron";
 import { Application } from "spectron";
 
 const beforeEach = function() {
-  this.timeout(10000);
   this.app = new Application({
     path: electron,
-    args: ["."],
-    startTimeout: 10000,
-    waitTimeout: 10000
+    args: ["."]
   });
   return this.app.start();
 };
 
 const afterEach = function() {
-  this.timeout(10000);
   if (this.app && this.app.isRunning()) {
     return this.app.stop();
   }
