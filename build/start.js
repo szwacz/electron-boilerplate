6951e92d15472187976538ed98cb9d572ce426bc
Jakub Szwacz <jakub@szwacz.com>
2021-05-26 07:12:30 +0200

    Clear terminal with each webpack rebuild

--- build/start.js
+++ build/start.js
@@ -1,10 +1,20 @@
 const childProcess = require("child_process");
+const readline = require("readline");
 const electron = require("electron");
 const webpack = require("webpack");
 const config = require("./webpack.app.config");
 
 const compiler = webpack(config({ development: true }));
 let electronStarted = false;
+
+const clearTerminal = () => {
+  if (process.stdout.isTTY) {
+    const blankLines = "\n".repeat(process.stdout.rows);
+    console.log(blankLines);
+    readline.cursorTo(process.stdout, 0, 0);
+    readline.clearScreenDown(process.stdout);
+  }
+};
 
 const watching = compiler.watch({}, (err, stats) => {
   if (err != null) {
@@ -18,5 +28,8 @@ const watching = compiler.watch({}, (err, stats) => {
       });
   }
 
-  console.log(stats.toString({ colors: true }));
+  if (stats != null) {
+    clearTerminal();
+    console.log(stats.toString({ colors: true }));
+  }
 });
